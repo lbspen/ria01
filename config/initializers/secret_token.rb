8bed23286ccbd799b755622a0c84a20a4198d246
lbs <lbspen@gmail.com>
2013-05-07 15:31:07 -0700

    completed examples from RIA Chapter 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '3aebfd49d905fab404d4c91c749ab75d3ae8c8efc4165a06b4dd0a16af02f9ae0e59bcdc74b67cdf021ba674c9de2b2f9612909c9f78c297c384f4f159269e24'
